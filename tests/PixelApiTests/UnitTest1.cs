13e6d3bdf1f65101f256a66e6096895598bddc9c
João Correia <j.correia@kigroup.de>
2024-02-24 20:07:36 +0000

    chore: project structure remake

---
+++ tests/PixelApiTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace PixelApiTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ tests/StorageServiceTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace StorageServiceTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
