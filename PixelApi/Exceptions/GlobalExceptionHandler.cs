b85ec8474a06735df7d96c1b8f5e6e0e189d3b1c
João Correia <j.correia@kigroup.de>
2024-02-24 18:32:33 +0000

    fix: fix docker file for dotnet8

---
+++ PixelApi/Exceptions/GlobalExceptionHandler.cs
@@ -0,0 +1,63 @@
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PixelApi.Exceptions;
+
+public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken
+    )
+    {
+        ProblemDetails problemDetails = exception switch
+        {
+            ArgumentException => HandleArgumentException(exception),
+            _ => HandleGenericException(exception)
+        };
+
+        httpContext.Response.StatusCode =
+            problemDetails.Status ?? (int)HttpStatusCode.InternalServerError;
+        httpContext.Response.ContentType = "application/problem+json";
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+
+    private ProblemDetails HandleGenericException(Exception exception)
+    {
+        var problemDetails = new ProblemDetails
+        {
+            Detail = exception.Message,
+            Status = (int)HttpStatusCode.InternalServerError,
+            Title = "Internal Server Error"
+        };
+
+        logger.LogError(
+            exception,
+            "An error occurred while processing the request. {Exception}",
+            exception
+        );
+
+        return problemDetails;
+    }
+
+    private ProblemDetails HandleArgumentException(Exception exception)
+    {
+        var problemDetails = new ProblemDetails
+        {
+            Detail = exception.Message,
+            Status = (int)HttpStatusCode.BadRequest,
+            Title = "Bad request"
+        };
+        logger.LogWarning(
+            exception,
+            "An error occurred while processing the request. {Exception}",
+            exception
+        );
+
+        return problemDetails;
+    }
+}
